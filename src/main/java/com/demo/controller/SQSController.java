a1bcf3d9b6626a58be28b732ac5fc20b30360682
maryannloves2runinthesun <62434233+maryannloves2runinthesun@users.noreply.github.com>
2020-07-08 14:52:32 -0400

    Updated queue name from properties file

--- src/main/java/com/demo/controller/SQSController.java
+++ src/main/java/com/demo/controller/SQSController.java
@@ -3,6 +3,7 @@ package com.demo.controller;
 import com.demo.model.TestData;
 import com.demo.service.SQSService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,12 @@ public class SQSController {
     @Autowired
     SQSService sqsService;
 
+    @Value( "${aws.queue.name}" )
+    private String awsQueueName;
+
     @PostMapping
     public void sendMessage(@RequestBody TestData testData) throws Exception {
-        sqsService.send("maryanns-queue", testData);
+        sqsService.send( awsQueueName, testData);
     };
 
 }
