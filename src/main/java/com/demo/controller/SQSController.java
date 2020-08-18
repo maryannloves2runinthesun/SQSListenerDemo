7884e1c8fc6322a2107b0b7aea4f52901f1750d1
Ron Jennings <rjennings@corporatewars.com>
2020-08-17 23:02:35 -0400

    removed build folder and added .gitignore

--- src/main/java/com/demo/controller/SQSController.java
+++ src/main/java/com/demo/controller/SQSController.java
@@ -1,11 +1,6 @@
 package com.demo.controller;
 
-import com.demo.model.TestData;
-import com.demo.service.SQSService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sqs")
 public class SQSController {
 
-    @Autowired
-    SQSService sqsService;
+//    @Autowired
+//    SQSService sqsService;
 
     @Value( "${aws.queue.name}" )
     private String awsQueueName;
 
-    @PostMapping
-    public void sendMessage(@RequestBody TestData testData) throws Exception {
-        sqsService.send( awsQueueName, testData);
-    };
+//    @PostMapping
+//    public void sendMessage(@RequestBody TestData testData) throws Exception {
+//        sqsService.send( awsQueueName, testData);
+//    };
 
 }
--- src/main/java/com/demo/service/SQSService.java
+++ src/main/java/com/demo/service/SQSService.java
@@ -1,9 +1,6 @@
 package com.demo.service;
 
-import com.demo.model.TestData;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Service;
@@ -12,15 +9,15 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class SQSService {
 
-    @Autowired
-    QueueMessagingTemplate messagingTemplate;
-
-    public void send(String topicName, TestData message) {
-        messagingTemplate.convertAndSend(topicName, message);
-    }
+//    @Autowired
+//    QueueMessagingTemplate messagingTemplate;
+//
+//    public void send(String topicName, TestData message) {
+//        messagingTemplate.convertAndSend(topicName, message);
+//    }
 
     @SqsListener(value = "${aws.queue.name}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-    public void queueListener(TestData message) throws Exception {
+    public void queueListener(String message) throws Exception {
 
         System.out.println("Message: " + message.toString());
 
